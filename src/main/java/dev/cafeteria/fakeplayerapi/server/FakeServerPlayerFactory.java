8056b455af7437bcf9270d2b06987b37a1e1adb5
PTOM76 <ptms76@outlook.jp>
2022-07-12 15:30:52 +0900

    Port to 1.19

--- src/main/java/dev/cafeteria/fakeplayerapi/server/FakeServerPlayerFactory.java
+++ src/main/java/dev/cafeteria/fakeplayerapi/server/FakeServerPlayerFactory.java
@@ -1,12 +1,14 @@
 package dev.cafeteria.fakeplayerapi.server;
 
 import com.mojang.authlib.GameProfile;
+import net.minecraft.network.encryption.PlayerPublicKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
+import org.jetbrains.annotations.Nullable;
 
 public interface FakeServerPlayerFactory {
 
     FakeServerPlayerFactory DEFAULT = FakeServerPlayer::new;
 
-    FakeServerPlayer create(FakePlayerBuilder builder, MinecraftServer server, ServerWorld world, GameProfile profile);
+    FakeServerPlayer create(FakePlayerBuilder builder, MinecraftServer server, ServerWorld world, GameProfile profile, @Nullable PlayerPublicKey publicKey);
 }
