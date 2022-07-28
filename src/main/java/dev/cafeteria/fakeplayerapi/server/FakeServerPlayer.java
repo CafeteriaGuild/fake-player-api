1578f69b94f8aee13b7378f324cd15f16c2fd88b
GabrielOlvH <gabrielh.oliveira222@gmail.com>
2022-07-28 20:02:18 -0300

    Update to 1.19

--- src/main/java/dev/cafeteria/fakeplayerapi/server/FakeServerPlayer.java
+++ src/main/java/dev/cafeteria/fakeplayerapi/server/FakeServerPlayer.java
@@ -3,16 +3,18 @@ package dev.cafeteria.fakeplayerapi.server;
 import com.mojang.authlib.GameProfile;
 import dev.cafeteria.fakeplayerapi.mixin.ServerPlayerEntityAccessor;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.network.encryption.PlayerPublicKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import org.jetbrains.annotations.Nullable;
 
 public class FakeServerPlayer extends ServerPlayerEntity {
 
     private final FakePlayerBuilder builder;
 
     protected FakeServerPlayer(FakePlayerBuilder builder, MinecraftServer server, ServerWorld world, GameProfile profile) {
-        super(server, world, profile);
+        super(server, world, profile, null);
         this.builder = builder;
 
         this.networkHandler = new FakeServerPlayNetworkHandler(server, FakeClientConnection.SERVER_FAKE_CONNECTION, this);
